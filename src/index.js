8760c1fc9f73932fe9b81a34c10f42c5354adcad
aprs <parszadeh.ahmad@gmail.com>
2019-11-06 14:58:38 +0330

    hello world

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// ReactDOM.render(<h1>This is my hello world</h1>, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
